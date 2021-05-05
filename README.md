9cdc5f7ccb6112e4cd2fab65a9bd38290b9404e2
Longqing Chen <lchen9@nd.edu>
2021-05-06 01:55:06 +0800

    Animation description

--- README.md
+++ README.md
@@ -19,6 +19,7 @@
 > The objective of this project is to build a GUI to simulate waveforms based on user-specified inputs over time. The user would be able to input 2 or 3 variables and the boolean operations they wish to perform on the variables. The GUI would take the input, perform the boolean operations, and output an animation of the waveforms in real time. Each variable will be a different color so it will be easier for the user to track each variable throughout its waveform. This GUI will include a component of animation which models the behavior of Boolean algebra and tools, such as K-maps, adders, decoders, multiplexers, etc. It could potentially help hardware engineers to address the problem of staring at a static screen for long periods of time. 
 
 **Project Deliverables**
+
 - [x] Boolean equations
 - [x] Basic GUI
 - [ ] Animated GUI
@@ -26,11 +27,10 @@
 **Development Log**
 - [x] Add simple evaluation feature, Longqing, 05/02/21
 - [x] Add simplification feature, Longqing, 05/04/21
+- [x] Add animation description
 - [ ] 
 
 **TODO List**
 
-- [ ] Dark/Light mode(labels)
-- [ ] K-map
-- [ ] Expression value animation
 - [ ] Waveform animation
+- [ ] 
--- tkinter_first.py
+++ tkinter_first.py
@@ -2,8 +2,9 @@
 
 from tkinter import *
 import tkinter.font as tkFont
-import boolean
+import time
 
+import boolean
 algebra = boolean.BooleanAlgebra()
 
 # Feel free to change colors(see TkinterColorCharts.png)
@@ -20,8 +21,7 @@ simpOutput2 = StringVar()
 # Configurations 
 window.title("animated waveform")
 window.geometry('700x700')
-window.minsize(700,700)
-window.maxsize(700,700)
+window.resizable(False,False)
 window.config(bg="white smoke")
 
 # Dark mode OR Light mode
@@ -30,26 +30,26 @@ modeInt.set(1)
 def change_mode():
     if modeInt.get() == 1:
         modeChange.set("light mode")
-        window.config(bg="gray35")
+        window.config(bg="gray30")
+        canvas.config(bg="gray35")
         modeInt.set(0)
     else:
         modeChange.set("dark mode")
         window.config(bg="white smoke")
+        canvas.config(bg="azure")
         modeInt.set(1)
 mode = Button(window,
-                command=change_mode,
-                activebackground = "LightSkyBlue1",
-                font=("consolas",11),
-                textvariable = modeChange,
-                width=10,
-                bd=0.15,
-                bg="SlateGray1",
-                relief="flat",
-                justify=CENTER).place(x=21,y=20)
+              command=change_mode,
+              activebackground = "LightSkyBlue1",
+              font=("consolas",11),
+              textvariable = modeChange,
+              width=11,
+              bd=0.15,
+              bg="SlateGray1",
+              relief="flat",
+              justify=CENTER).place(x=21,y=530)
 
-# Labels
 # Simple Evaluation: input two expression strings, output whether they are equal to each other
-simpleEvaluation = Label(window,font=("consolas",11),text = "simple evaluation",width=20,bd=0.2,relief="flat").place(x=20,y=70)
 
 def sim_eval():
     expr1 = algebra.parse(inputExpr1.get())
@@ -59,48 +59,83 @@ def sim_eval():
     else:
         evalD.set("The input expressions are not equal.")
 input1 = Label(window,
-                font=("consolas",11),
-                text = "input1",
-                width=11,
-                bd=0.2,
-                bg="SlateGray1",
-                relief="flat").place(x=20,y=100)
+               font=("consolas",11),
+               text = "input1",
+               width=11,
+               bd=0.2,
+               bg="SlateGray1",
+               relief="flat").place(x=20,y=570)
 entry1 = Entry(window,
-                font=("consolas bold",11),
-                justify=LEFT,
-                relief=SUNKEN,
-                textvariable=inputExpr1).place(x=150,y=100)
+              font=("consolas bold",11),
+              justify=LEFT,
+              relief=SUNKEN,
+              textvariable=inputExpr1).place(x=150,y=570)
 input2 = Label(window,
-                font=("consolas",11),
-                text = "input2",
-                width=11,
-                bd=0.2,
-                bg="SlateGray1",
-                relief="flat").place(x=20,y=130)
+               font=("consolas",11),
+               text = "input2",
+               width=11,
+               bd=0.2,
+               bg="SlateGray1",
+               relief="flat").place(x=20,y=600)
 entry2 = Entry(window,
-                font=("consolas bold",11),
-                justify=LEFT,
-                relief=SUNKEN,
-                textvariable=inputExpr2).place(x=150,y=130)
+              font=("consolas bold",11),
+              justify=LEFT,
+              relief=SUNKEN,
+              textvariable=inputExpr2).place(x=150,y=600)
 simpEva = Button(window,
-                    command=sim_eval,
-                    activebackground = "LightSkyBlue1",
-                    font=("consolas italic",11),
-                    text="Evaluate!",
-                    width=11,
-                    bd=0.1,
-                    bg="SlateGray1",
-                    relief="flat",
-                    justify=CENTER).place(x=20,y=160)
+                 command=sim_eval,
+                 activebackground = "LightSkyBlue1",
+                 font=("consolas italic",11),
+                 text="Evaluate!",
+                 width=11,
+                 bd=0.1,
+                 bg="SlateGray1",
+                 relief="flat",
+                 justify=CENTER).place(x=20,y=640)
 evalDisplay = Label(window,
-                    font=("consolas",11),
-                    textvariable=evalD,
-                    width=43,
-                    bd=4,
-                    justify=LEFT,
-                    relief="flat").place(x=150,y=160)
+               font=("consolas",11),
+               textvariable=evalD,
+               width=40,
+               bd=4,
+               justify=LEFT,
+               relief="flat").place(x=150,y=640)
 
-# Simplification: when clicked, display a simplied expression
+# Evaluation animation: when clicked, display a waveform of the two input expressions
+animation = Button(window,
+                 command=Graph,
+                 activebackground = "LightSkyBlue1",
+                 font=("consolas italic",11),
+                 text="Generate waveform!",
+                 width=20,
+                 bd=0.1,
+                 bg="SlateGray1",
+                 relief="flat",
+                 justify=CENTER).place(x=500,y=640)
+def Graph(step=0.001):
+    '''
+    Delete later
+    # Parameters for graph: input expression 1, input expression 2, dark or light mode
+    # Get symbols
+    # Generate the waveform of the the two expressions 
+    for instance e1 = x&y, e2 = x|y
+        1s  2s  3s  4s
+    x   0   1   1   0
+    y   0   0   1   1
+    e1  0   0   1   0
+    e2  0   1   1   1
+    # End of animation
+
+    '''
+    canvas.create_line(0,0,200,200,fill='black')
+    canvas.update()
+canvas = Canvas(window,
+                width=700,
+                height=500,
+                bg="azure",
+                relief=SUNKEN)
+canvas.pack()
+canvas.bind('<1>',change_mode)
+
 # Simplification: when clicked, display a simplied expression
 def simplify_one():
     expr1 = algebra.parse(inputExpr1.get())
@@ -109,41 +144,36 @@ def simplify_two():
     expr2 = algebra.parse(inputExpr2.get())
     simpOutput2.set(expr2.simplify())
 simplify1 = Button(window,
-                    command=simplify_one,
-                    activebackground = "LightSkyBlue1",
-                    font=("consolas italic",10),
-                    text="Simplify!",
-                    width=10,
-                    bg="SlateGray1",
-                    relief="flat",
-                    justify=CENTER).place(x=330,y=100)
+                 command=simplify_one,
+                 activebackground = "LightSkyBlue1",
+                 font=("consolas italic",10),
+                 text="Simplify!",
+                 width=10,
+                 bg="SlateGray1",
+                 relief="flat",
+                 justify=CENTER).place(x=330,y=570)
 simplify2 = Button(window,
-                    command=simplify_two,
-                    activebackground = "LightSkyBlue1",
-                    font=("consolas italic",10),
-                    text="Simplify!",
-                    width=10,
-                    bg="SlateGray1",
-                    relief="flat",
-                    justify=CENTER).place(x=330,y=130)
+                 command=simplify_two,
+                 activebackground = "LightSkyBlue1",
+                 font=("consolas italic",10),
+                 text="Simplify!",
+                 width=10,
+                 bg="SlateGray1",
+                 relief="flat",
+                 justify=CENTER).place(x=330,y=600)
 simplifyOutput1 = Label(window,
-                    font=("consolas",11),
-                    textvariable = simpOutput1,
-                    width=15,
-                    bd=0.2,
-                    relief="flat").place(x=450,y=100)
+                   font=("consolas",11),
+                   textvariable = simpOutput1,
+                   width=15,
+                   bd=0.2,
+                   relief="flat").place(x=450,y=570)
 simplifyOutput2 = Label(window,
-                    font=("consolas",11),
-                    textvariable = simpOutput2,
-                    width=15,
-                    bd=0.2,
-                    relief="flat").place(x=450,y=130)
+                   font=("consolas",11),
+                   textvariable = simpOutput2,
+                   width=15,
+                   bd=0.2,
+                   relief="flat").place(x=450,y=600)
 
-# Kmap (optional): when clicked, display Kmaps of the expressions
-kmap = Button(window,command=k_map)
-def k_map():
-    '''
-    '''
 
 window.mainloop()
 
