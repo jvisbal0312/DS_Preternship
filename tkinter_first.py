82774158d5849f0f9d636d893cd977e163dca88d
Longqing Chen <lchen9@nd.edu>
2021-05-08 03:24:55 +0800

    1 input animation

--- tkinter_first.py
+++ tkinter_first.py
@@ -50,7 +50,7 @@ mode = Button(window,
               justify=CENTER).place(x=21,y=530)
 
 # Simple Evaluation: input two expression strings, output whether they are equal to each other
-
+# Instructions
 def sim_eval():
     expr1 = algebra.parse(inputExpr1.get())
     expr2 = algebra.parse(inputExpr2.get())
@@ -101,6 +101,78 @@ evalDisplay = Label(window,
                relief="flat").place(x=150,y=640)
 
 # Evaluation animation: when clicked, display a waveform of the two input expressions
+def Graph():
+    
+    t1 = time.time()
+    global canvas
+    
+    if modeInt.get() == 1:
+        rect = canvas.create_rectangle(40, 100, 1000, 320, 
+                                fill="azure",
+                                outline="")
+        canvas.create_text(230,70,
+                           text="waveforms of the two input expressions",
+                           fill="RoyalBlue1",
+                           font=("consolas bold",15))
+        canvas.create_text(25,130,
+                          text="1",
+                          fill="RoyalBlue1",
+                          font=("consolas bold",15))
+        canvas.create_text(25,300,
+                          text="0",
+                          fill="RoyalBlue1",
+                          font=("consolas bold",15))
+    else:
+        rect = canvas.create_rectangle(40, 100, 1000, 300, 
+                                fill="gray35",
+                                outline="")
+        canvas.create_text(230,70,
+                           text="waveforms of the two input expressions",
+                           fill="mint cream",
+                           font=("consolas bold",15))
+        canvas.create_text(25,130,
+                          text="1",
+                          fill="mint cream",
+                          font=("consolas bold",15))
+        canvas.create_text(25,300,
+                          text="0",
+                          fill="mint cream",
+                          font=("consolas bold",15))
+    global symbol_list1
+    global symbol_list2
+    symbol_list1=algebra.parse(inputExpr1.get()).get_symbols()
+    expr1 = algebra.parse(inputExpr1.get())
+    symbol_list2=algebra.parse(inputExpr2.get()).get_symbols()
+    expr2 = algebra.parse(inputExpr2.get())
+    if len(symbol_list1)==2:
+        pass
+    elif len(symbol_list1)==1:
+        if symbol_list1[0]==symbol_list2[0]:
+            if modeInt.get() == 1:
+                canvas.create_line(40,300,330,300,330,130,630,130,width=2,fill="RoyalBlue1")
+            else:
+                canvas.create_line(40,300,330,300,330,130,630,130,width=2,fill="mint cream")
+        else:
+            if modeInt.get() == 1:
+                canvas.create_line(40,300,330,300,330,130,630,130,width=2,fill="RoyalBlue1")
+                canvas.create_line(40,130,330,130,330,300,630,300,width=2,fill="deep pink")
+            else:
+                canvas.create_line(40,300,330,300,330,130,630,130,width=2,fill="mint cream")
+                canvas.create_line(40,130,330,130,330,300,630,300,width=2,fill="deep pink")
+    else:
+        canvas.create_text(330,400,
+                          text="Sorry, we currently support expression waveforms of up to 2 variables.",
+                          fill="RoyalBlue1",
+                          font=("consolas italic",13))
+     
+    canvas.tag_raise(rect)
+    for i in range(0,150):
+        canvas.move(rect,5,0)
+        window.update()
+        time.sleep(0.07)
+    
+    
+
 animation = Button(window,
                  command=Graph,
                  activebackground = "LightSkyBlue1",
@@ -111,23 +183,6 @@ animation = Button(window,
                  bg="SlateGray1",
                  relief="flat",
                  justify=CENTER).place(x=500,y=640)
-def Graph(step=0.001):
-    '''
-    Delete later
-    # Parameters for graph: input expression 1, input expression 2, dark or light mode
-    # Get symbols
-    # Generate the waveform of the the two expressions 
-    for instance e1 = x&y, e2 = x|y
-        1s  2s  3s  4s
-    x   0   1   1   0
-    y   0   0   1   1
-    e1  0   0   1   0
-    e2  0   1   1   1
-    # End of animation
-
-    '''
-    canvas.create_line(0,0,200,200,fill='black')
-    canvas.update()
 canvas = Canvas(window,
                 width=700,
                 height=500,
@@ -135,6 +190,7 @@ canvas = Canvas(window,
                 relief=SUNKEN)
 canvas.pack()
 canvas.bind('<1>',change_mode)
+
 
 # Simplification: when clicked, display a simplied expression
 def simplify_one():
