023db0cba0e46bbee1b0856360060a370ae65750
Longqing Chen <lchen9@nd.edu>
2021-05-10 21:47:35 +0800

    Update tkinter_first.py
    - the lineDisplay function is a bit messy and needs debugging (sorry guys I can't make it to the coding craze if you have any questions i can always zoom)

--- tkinter_first.py
+++ tkinter_first.py
@@ -101,8 +101,179 @@ evalDisplay = Label(window,
                relief="flat").place(x=150,y=640)
 
 # Evaluation animation: when clicked, display a waveform of the two input expressions
+def parseToZero(expr,integer=0):
+    string = []
+    for i in expr:
+        if i.isalpha():
+            string.append(i)
+    string = list(set(string))
+    for i in expr:
+        if i == string[integer]:
+            expr = expr.replace(i,"0")
+    return expr
+def parseToOne(expr,integer=0):
+    string = []
+    for i in expr:
+        if i.isalpha():
+            string.append(i)
+    string = list(set(string))
+    for i in expr:
+        if i == string[integer]:
+            expr = expr.replace(i,"1")
+    return expr
+
+def lineDisplay(t1,t2,TempExpr1,TempExpr2,seg=2):
+    startX = int(40 + (seg-1)*160)
+    endX = int(40 + (seg)*160)
+    # Specify the starting_x and ending_x
+    if t1 == 0: # prev1 = 0
+        if type(TempExpr1) == FALSE: # prev1 = 0, curr1 = 0
+            t1 = 0 
+            if modeInt.get() == 1: # color scheme(light)
+                if type(TempExpr2) == FALSE: # curr2 = 0
+                    if t2 == 1: # expr1 from 0 to 0, expr2 from 1 to 0
+                        canvas.create_line(startX,130,startX,300,endX,300,width=2,fill="deep pink")
+                    else: # expr1 from 0 to 0, expr2 from 0 to 0
+                        canvas.create_line(startX,300,endX,300,width=2,fill="deep pink")
+                    t2 = 0
+                else: # current truth value of expr2 is 1
+                    if t2 == 1: # expr1 from 0 to 0, expr2 from 1 to 1
+                        canvas.create_line(startX,300,endX,300,width=2,fill="RoyalBlue1")
+                        canvas.create_line(startX,130,endX,130,width=2,fill="deep pink")
+                    else: # expr1 from 0 to 0, expr2 from 0 to 1
+                        canvas.create_line(startX,300,endX,300,width=2,fill="RoyalBlue1")
+                        canvas.create_line(startX,300,startX,130,endX,130,width=2,fill="deep pink")
+                    t2 = 1
+            else: # color scheme(dark)
+                if type(TempExpr2) == FALSE: # current truth value of expr2 is 0
+                    if t2 == 1: # expr1 from 0 to 0, expr2 from 1 to 0
+                        canvas.create_line(startX,130,startX,300,endX,300,width=2,fill="deep pink")
+                    else: # expr1 from 0 to 0, expr2 from 0 to 0
+                        canvas.create_line(startX,300,endX,300,width=2,fill="deep pink")
+                    t2 = 0
+                else: # current truth value of expr2 is 1
+                    if t2 == 1: # expr1 from 0 to 0, expr2 from 1 to 1
+                        canvas.create_line(startX,130,endX,130,width=2,fill="deep pink")
+                        canvas.create_line(startX,300,endX,300,width=2,fill="mint cream")
+                    else: # expr1 from 0 to 0, expr2 from 0 to 1
+                        canvas.create_line(startX,300,startX,130,endX,130,width=2,fill="deep pink")
+                        canvas.create_line(startX,300,endX,300,width=2,fill="mint cream")
+                    t2 = 1
+        else: # prev1 = 0, curr1 = 1
+            t1 = 1 
+            if modeInt.get() == 1: # color scheme(light)
+                if type(TempExpr2) == FALSE: # curr2 = 0
+                    if t2 == 1: # expr1 from 0 to 1, expr2 from 1 to 0
+                        canvas.create_line(startX,130,startX,300,endX,300,width=2,fill="deep pink")
+                        canvas.create_line(startX,130,endX,130,width=2,fill="RoyalBlue1")
+                    else: # expr1 from 0 to 1, expr2 from 0 to 0
+                        canvas.create_line(startX,300,endX,300,width=2,fill="deep pink")
+                        canvas.create_line(startX,300,startX,130,endX,130,width=2,fill="RoyalBlue1")
+                    t2 = 0
+                else: # curr2 = 1
+                    if t2 == 1: # expr1 from 0 to 1, expr2 from 1 to 1
+                        canvas.create_line(startX,130,endX,130,width=2,fill="deep pink")
+                        canvas.create_line(startX,300,endX,300,width=2,fill="RoyalBlue1")
+                    else: # expr1 from 0 to 1, expr2 from 0 to 1
+                        canvas.create_line(startX,300,startX,130,endX,130,width=2,fill="deep pink")
+                    t2 = 1
+            else: # color scheme(dark)
+                if type(TempExpr2) == FALSE: # curr2 = 0
+                    if t2 == 1: # expr1 from 0 to 1, expr2 from 1 to 0
+                        canvas.create_line(startX,130,startX,300,endX,300,width=2,fill="deep pink")
+                        canvas.create_line(startX,130,endX,130,width=2,fill="mint cream")
+                    else: # expr1 from 0 to 1, expr2 from 0 to 0
+                        canvas.create_line(startX,300,endX,300,width=2,fill="deep pink")
+                        canvas.create_line(startX,300,startX,130,endX,130,width=2,fill="mint cream")
+                    t2 = 0
+                else: # curr2 = 1
+                    if t2 == 1: # expr1 from 0 to 1, expr2 from 1 to 1
+                        canvas.create_line(startX,130,endX,130,width=2,fill="deep pink")
+                        canvas.create_line(startX,300,endX,300,width=2,fill="mint cream")
+                    else: # expr1 from 0 to 1, expr2 from 0 to 1
+                        canvas.create_line(startX,300,startX,130,endX,130,width=2,fill="deep pink")
+                    t2 = 1
+    else: # prev1 = 1
+        if type(TempExpr1) == FALSE: # prev1 = 1, curr1 = 0
+            t1 = 0
+            if modeInt.get() == 1: # color scheme(light)
+                if type(TempExpr2) == FALSE: # curr2 = 0
+                    if t2 == 1: # expr1 from 1 to 0, expr2 from 1 to 0
+                        canvas.create_line(startX,130,startX,300,endX,300,width=2,fill="deep pink")
+                    else: # expr1 from 1 to 0, expr2 from 0 to 0
+                        canvas.create_line(startX,300,endX,300,width=2,fill="deep pink")
+                        canvas.create_line(startX,130,startX,300,width=2,fill="RoyalBlue1")
+                    t2 = 0
+                else: # curr2 = 1
+                    if t2 == 1: # expr1 from 1 to 0, expr2 from 1 to 1
+                        canvas.create_line(startX,130,endX,130,width=2,fill="deep pink")
+                        canvas.create_line(startX,130,startX,300,endX,300,width=2,fill="RoyalBlue1")
+                    else: # expr1 from 1 to 0, expr2 from 0 to 1
+                        canvas.create_line(startX,300,startX,130,endX,130,width=2,fill="deep pink")
+                        canvas.create_line(startX,300,endX,300,width=2,fill="RoyalBlue1")
+                    t2 = 1
+            else: # color scheme(dark)
+                if type(TempExpr2) == FALSE: # curr2 = 0
+                    if t2 == 1: # expr1 from 1 to 0, expr2 from 1 to 0
+                        canvas.create_line(startX,130,startX,300,endX,300,width=2,fill="deep pink")
+                    else: # expr1 from 1 to 0, expr2 from 0 to 0
+                        canvas.create_line(startX,300,endX,300,width=2,fill="deep pink")
+                        canvas.create_line(startX,130,startX,300,width=2,fill="mint cream")
+                    t2 = 0
+                else: # curr2 = 1
+                    if t2 == 1: # expr1 from 1 to 0, expr2 from 1 to 1
+                        canvas.create_line(startX,130,endX,130,width=2,fill="deep pink")
+                        canvas.create_line(startX,130,startX,300,endX,300,width=2,fill="mint cream")
+                    else: # expr1 from 1 to 0, expr2 from 0 to 1
+                        canvas.create_line(startX,300,startX,130,endX,130,width=2,fill="deep pink")
+                        canvas.create_line(startX,300,endX,300,width=2,fill="mint cream")
+                    t2 = 1
+        else: # prev1 = 1, curr1 = 1
+            t1 = 1
+            if modeInt.get() == 1: # color scheme(light)
+                if type(TempExpr2) == FALSE: # curr2 = 0
+                    if t2 == 1: # expr1 from 1 to 1, expr2 from 1 to 0
+                        canvas.create_line(startX,130,startX,300,endX,300,width=2,fill="deep pink")
+                        canvas.create_line(startX,130,endX,130,width=2,fill="RoyalBlue1")
+                    else: # expr1 from 1 to 1, expr2 from 0 to 0
+                        canvas.create_line(startX,300,endX,300,width=2,fill="deep pink")
+                        canvas.create_line(startX,130,endX,130,width=2,fill="RoyalBlue1")
+                    t2 = 0
+                else: # curr2 = 1
+                    if t2 == 1: # expr1 from 1 to 1, expr2 from 1 to 1
+                        canvas.create_line(startX,130,endX,130,width=2,fill="deep pink")
+                    else: # expr1 from 1 to 1, expr2 from 0 to 1
+                        canvas.create_line(startX,300,startX,130,endX,130,width=2,fill="deep pink")
+                    t2 = 1
+            else: # color scheme(dark)
+                if type(TempExpr2) == FALSE: # curr2 = 0
+                    if t2 == 1: # expr1 from 1 to 1, expr2 from 1 to 0
+                        canvas.create_line(startX,130,startX,300,endX,300,width=2,fill="deep pink")
+                        canvas.create_line(startX,130,endX,130,width=2,fill="mint cream")
+                    else: # expr1 from 1 to 1, expr2 from 0 to 0
+                        canvas.create_line(startX,300,endX,300,width=2,fill="deep pink")
+                        canvas.create_line(startX,130,endX,130,width=2,fill="mint cream")
+                    t2 = 0
+                else: # curr2 = 1
+                    if t2 == 1: # expr1 from 1 to 1, expr2 from 1 to 1
+                        canvas.create_line(startX,130,endX,130,width=2,fill="deep pink")
+                    else: # expr1 from 1 to 1, expr2 from 0 to 1
+                        canvas.create_line(startX,300,startX,130,endX,130,width=2,fill="deep pink")
+                    t2 = 1
+    return t1,t2
+
+def printAlert():
+    if modeInt.get() == 1:
+        canvas.create_text(330,400,
+                              text="Sorry, we currently support expression waveforms of up to 2 variables.",
+                              fill="RoyalBlue1",
+                              font=("consolas bold",13))
+    else:
+        canvas.create_text(330,400,
+                              text="Sorry, we currently support expression waveforms of up to 2 variables.",
+                              fill="mint cream",
+                              font=("consolas bold",13))
 def Graph():
-    
     t1 = time.time()
     global canvas
     
@@ -123,7 +294,9 @@ def Graph():
                           fill="RoyalBlue1",
                           font=("consolas bold",15))
     else:
-        rect = canvas.create_rectangle(40, 100, 1000, 320,fill="gray35",outline="")
+        rect = canvas.create_rectangle(40, 100, 1000, 320,
+                                       fill="gray35",
+                                       outline="")
         canvas.create_text(230,70,
                            text="waveforms of the two input expressions",
                            fill="mint cream",
@@ -138,13 +311,94 @@ def Graph():
                           font=("consolas bold",15))
     global symbol_list1
     global symbol_list2
+    
     symbol_list1=algebra.parse(inputExpr1.get()).get_symbols()
     expr1 = algebra.parse(inputExpr1.get())
     symbol_list2=algebra.parse(inputExpr2.get()).get_symbols()
     expr2 = algebra.parse(inputExpr2.get())
-    if len(symbol_list1)==2:
-        pass
-    elif len(symbol_list1)==1:
+    if len(set(symbol_list1))==2 and len(set(symbol_list2))==2:
+        if symbol_list1 == symbol_list2:
+            t1 = 0
+            t2 = 0
+            # First line
+            Temp1 = parseToZero(inputExpr1.get(),0)
+            Temp1 = parseToZero(Temp1,0)
+            TempExpr1 = algebra.parse(Temp1).simplify()
+
+            Temp2 = parseToZero(inputExpr2.get(),0)
+            Temp2 = parseToOne(Temp2,0)
+            TempExpr2 = algebra.parse(Temp2)
+
+            if type(TempExpr1) == FALSE:
+                t1 = 0
+                if type(TempExpr2  == FALSE):
+                    t2 = 0
+                    canvas.create_line(40,300,200,300,width=2,fill="deep pink")
+                    if modeInt.get() == 1:
+                        canvas.create_line(40,300,200,300,width=2,fill="RoyalBlue1")
+                    else:
+                        canvas.create_line(40,300,200,300,width=2,fill="mint cream")
+                elif type(TempExpr2 == TRUE):
+                    t2 = 1
+                    canvas.create_line(40,130,200,130,width=2,fill="deep pink")
+                    if modeInt.get() == 1:
+                        canvas.create_line(40,300,200,300,width=2,fill="RoyalBlue1")
+                    else:
+                        canvas.create_line(40,300,200,300,width=2,fill="mint cream")
+            elif type(TempExpr1) == TRUE:
+                t1 = 1
+                if type(TempExpr2  == FALSE):
+                    t2 = 0
+                    canvas.create_line(40,300,200,300,width=2,fill="deep pink")
+                    if modeInt.get() == 1:
+                        canvas.create_line(40,130,200,130,width=2,fill="RoyalBlue1")
+                    else:
+                        canvas.create_line(40,130,200,130,width=2,fill="mint cream")
+                elif type(TempEXpr2 == TRUE):
+                    t2 = 1
+                    canvas.create_line(40,130,200,130,width=2,fill="deep pink")
+                    if modeInt.get() == 1:
+                        canvas.create_line(40,130,200,130,width=2,fill="RoyalBlue1")
+                    else:
+                        canvas.create_line(40,130,200,130,width=2,fill="mint cream")
+            
+            # Second line
+            Temp1 = parseToZero(inputExpr1.get(),0)
+            Temp1 = parseToOne(Temp1,0)
+            TempExpr1 = algebra.parse(Temp1).simplify()
+
+            Temp2 = parseToZero(inputExpr2.get(),0)
+            Temp2 = parseToOne(Temp2,0)
+            TempExpr2 = algebra.parse(Temp2)
+            
+            t1,t2 = lineDisplay(t1,t2,TempExpr1,TempExpr2,2)
+            
+            
+            #Third line
+            Temp1 = parseToOne(inputExpr1.get(),0)
+            Temp1 = parseToOne(Temp1,0)
+            TempExpr1 = algebra.parse(Temp1).simplify()
+
+            Temp2 = parseToOne(inputExpr2.get(),0)
+            Temp2 = parseToOne(Temp2,0)
+            TempExpr2 = algebra.parse(Temp2)
+            
+            t1,t2 = lineDisplay(t1,t2,TempExpr1,TempExpr2,3)
+            
+            #Fourth line
+            Temp1 = parseToOne(inputExpr1.get(),0)
+            Temp1 = parseToZero(Temp1,0)
+            TempExpr1 = algebra.parse(Temp1).simplify()
+
+            Temp2 = parseToOne(inputExpr2.get(),0)
+            Temp2 = parseToZero(Temp2,0)
+            TempExpr2 = algebra.parse(Temp2)
+            
+            t1,t2 = lineDisplay(t1,t2,TempExpr1,TempExpr2,4)
+                
+        else:
+            printAlert()
+    elif len(symbol_list1)==1 and len(symbol_list2)==1:
         if symbol_list1[0]==symbol_list2[0]:
             if modeInt.get() == 1:
                 canvas.create_line(40,300,330,300,330,130,630,130,width=2,fill="RoyalBlue1")
@@ -158,10 +412,7 @@ def Graph():
                 canvas.create_line(40,300,330,300,330,130,630,130,width=2,fill="mint cream")
                 canvas.create_line(40,130,330,130,330,300,630,300,width=2,fill="deep pink")
     else:
-        canvas.create_text(330,400,
-                          text="Sorry, we currently support expression waveforms of up to 2 variables.",
-                          fill="RoyalBlue1",
-                          font=("consolas italic",13))
+        printAlert()
      
     canvas.tag_raise(rect)
     for i in range(0,150):
